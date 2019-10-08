7d03d56838a58801701ad40b42fa712327d4176e
wangqiubao <wangqiubao@u-care.net>
2019-10-08 10:37:59 +0800

    增加kafka+storm demo

--- src/main/java/com/criss/wang/storm/service/KafkaBolt.java
+++ src/main/java/com/criss/wang/storm/service/KafkaBolt.java
@@ -4,7 +4,6 @@ import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
-//import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
